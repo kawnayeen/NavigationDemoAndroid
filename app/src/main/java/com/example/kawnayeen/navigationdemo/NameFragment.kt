29e85cf4396926b65e658997426cb25a32fc6cd8
kawnayeen <kawnayeen.08@gmail.com>
2018-10-10 11:24:02 +0600

    name fragment added and transition from account to name happenning

--- app/src/main/java/com/example/kawnayeen/navigationdemo/NameFragment.kt
+++ app/src/main/java/com/example/kawnayeen/navigationdemo/NameFragment.kt
@@ -6,27 +6,20 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.fragment_name.*
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class AccountFragment : Fragment() {
+class NameFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return inflater.inflate(R.layout.fragment_name, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        nextBtn.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putString("nameArg", nameEt.text.toString())
-            it.findNavController().navigate(R.id.nameFragment, bundle)
-        }
+        nameTv.text = arguments?.getString("nameArg")
     }
+
+
 }
